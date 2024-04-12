ea2e81def68169eb020f6656b67b198269a14fbf
gabriel <gabrielqap@hotmail.com>
2024-04-12 15:22:53 -0300

    Removed unused libraries

--- src/main/java/br/com/api/joyapi/service/AuthorizationService.java
+++ src/main/java/br/com/api/joyapi/service/AuthorizationService.java
@@ -1,8 +1,6 @@
 package br.com.api.joyapi.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,8 +8,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.api.joyapi.entity.User;
-import br.com.api.joyapi.entity.dto.AuthenticationDTO;
-import br.com.api.joyapi.entity.dto.LoginResponseDTO;
 import br.com.api.joyapi.entity.dto.RegisterDTO;
 import br.com.api.joyapi.repository.UserRepository;
 
